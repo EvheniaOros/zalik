cd057ab024a05fc2a0b13367fdc95fae361f7d52
OROSEV <zhenka-oros@mail.ru>
2018-12-05 00:07:47 +0200

    second

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+  	@write - Write.all
   end
 end
--- app/controllers/writes_controller.rb
+++ app/controllers/writes_controller.rb
@@ -1,25 +1,30 @@
 class WritesController < ApplicationController
-	before_action :authenticate_user!
-  	before_action :find_post, only: [:show, :edit, :update, :destroy]
-  def index
-  end
-def new
-  end
-def create
-  end
-def show
-  end
-def edit
-  end
-def update
-  end
-def destroy
-  end
-def find_post
-  end
-private
-private 
-  def post_params
-  	
-  end
+before_action :authenticate_user!
+before_action :find_post, only: [:show, :edit, :update, :destroy]
+   def index
+   end
+   def new
+    @write = Write.new
+	end
+	def create
+		@write = current_user.writes.build(write_params)
+  	if @write.save
+  		redirect_to root_path
+  	end
+	end
+    def show
+	end
+	def edit
+	end
+	def update
+	end
+	def destroy
+	end
+	def find_post
+		@write = Write.find_by(id: params[:id])
+	end
+	private
+	def post_params
+		params.require(:write).permit(:title, :firstheroy, :secondheroy, :aboutstory, :body)	
+	end
 end
