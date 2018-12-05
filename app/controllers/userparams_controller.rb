6f5946d31cd706fdde6d4ffcd863980e6291151e
OROSEV <zhenka-oros@mail.ru>
2018-12-05 20:54:24 +0200

    profile

--- app/controllers/userparams_controller.rb
+++ app/controllers/userparams_controller.rb
@@ -6,11 +6,24 @@ class UserparamsController < ApplicationController
   	@userparam = Userparam.new
   end
   def create
+    @userparam = current_user.build_userparam(profile_params)
+      if @userparam.save
+      redirect_to userparam_path(@userparam.id)
+      end
   end
   def edit
+     @userparam = current_user.userparam
   end
   def update
+    @userparam = Userparam.find_by(user_id: current_user.id)
+     if @userparam.update(profile_params)
+      redirect_to userparam_path(@userparam.id)
+     end
   end
   def destroy
+  end
+  private
+  def profile_params
+    params.require(:userparam).permit(:user_id, :firstname, :lastname, :age)
   end
 end
