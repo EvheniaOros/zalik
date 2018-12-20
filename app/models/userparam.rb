c768e3dcd69148f45ed8db797727573d413ac0de
OROSEV <zhenka-oros@mail.ru>
2018-12-20 18:23:17 +0200

    add agevalidator

--- app/models/userparam.rb
+++ app/models/userparam.rb
@@ -3,18 +3,8 @@ class Userparam < ApplicationRecord
 	validates :firstname, :lastname, :age, presence: true
 
 	validates  :age, numericality: { only_integer: true }
-  
+  validates_inclusion_of :age, :in => 18..45
 
-	  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  
- 
-  def age_under
-    if age < 17
-      errors.add(:age, "too young")
-    end
-    if age > 45
-    		errors.add(:age, "too old")
-    end
-  end
 end
